6c9d7a2b7bf3ddad8687bb9558f459df44209949
Yuri Luttsev <floomyz@vk.com>
2025-05-17 00:00:59 +0300

    Изменены основные методы в ProductImageService

--- src/main/java/ru/example/catalogservice/service/ProductImageService.java
+++ src/main/java/ru/example/catalogservice/service/ProductImageService.java
@@ -1,54 +1,82 @@
 package ru.example.catalogservice.service;
 
-import jakarta.transaction.Transactional;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.example.catalogservice.model.entity.Product;
 import ru.example.catalogservice.model.entity.ProductImage;
-import ru.example.catalogservice.model.event.product.ProductEvent;
-import ru.example.catalogservice.model.event.product.ProductEventFactory;
-import ru.example.catalogservice.model.event.productimage.ProductImageEvent;
-import ru.example.catalogservice.model.event.productimage.ProductImageEventFactory;
 import ru.example.catalogservice.repository.ProductImageRepository;
-import ru.example.catalogservice.repository.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductImageService {
-    private final ProductImageRepository imageRepository;
-    private final ProductRepository productRepository;
-    private final KafkaTemplate<String, ProductImageEvent> kafkaTemplate;
 
-    @Transactional
-    public ProductImage attachImage(UUID productId, String imageUrl) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found with id: " + productId));
+    private final ProductImageRepository productImageRepository;
+    private final MinioClient minioClient;
 
-        ProductImage image = ProductImage.builder()
-                .product(product)
-                .imageUrl(imageUrl)
-                .isActive(true)
-                .build();
+    @Value("${product.max-images}")
+    private Integer maxImages;
 
-        ProductImage saved = imageRepository.save(image);
-        ProductImageEvent event = ProductImageEventFactory.createEvent(saved, ProductImageEventFactory.EventType.ATTACHED);
-        kafkaTemplate.send("product-image-events", image.getId().toString(), event);
+    @Value("${minio.endpoint}")
+    private String endpoint;
 
-        return saved;
+    @Value("${minio.bucket}")
+    private String bucket;
+
+    public void attachImagesToProduct(Product product, List<String> imagesUrls) {
+        List<ProductImage> productImages = imagesUrls.stream()
+                .map(imageUrl -> ProductImage.builder()
+                        .product(product)
+                        .fileName(imageUrl)
+                        .build())
+                .toList();
+        productImageRepository.saveAll(productImages);
     }
 
-    @Transactional
-    public void detachImage(UUID imageId) {
-        ProductImage image = imageRepository.findById(imageId)
-                .orElseThrow(() -> new RuntimeException("Product image not found with id: " + imageId));
+    public List<String> saveImagesInFileStorage(List<MultipartFile> images) {
+        List<String> imageUrls = new ArrayList<>();
+        int size = Math.min(images.size(), maxImages);
+        for (int i = 0; i < size; i++) {
+            MultipartFile file = images.get(i);
+            if (!file.isEmpty() && isImage(file)) {
+                String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+                String imageName = "%s.%s".formatted(UUID.randomUUID(), extension);
+                try {
+                    minioClient.putObject(PutObjectArgs.builder()
+                            .object(imageName)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .build());
+                } catch (Exception e) {
+                    log.error("Error when saving an image to MinIO: {}", e.getMessage());
+                }
+                imageUrls.add(imageName);
+            }
+        }
+        return imageUrls;
+    }
 
-        image.setIsActive(false);
-        ProductImage saved = imageRepository.save(image);
+    public List<String> getImagesUrls(List<ProductImage> productImages) {
+        return productImages.stream()
+                .map(productImage -> "%s/%s/%s".formatted(endpoint, bucket, productImage.getFileName()))
+                .toList();
+    }
 
-        ProductImageEvent event = ProductImageEventFactory.createEvent(saved, ProductImageEventFactory.EventType.ATTACHED);
-        kafkaTemplate.send("product-image-events", image.getId().toString(), event);
+    private boolean isImage(MultipartFile file) {
+        try {
+            String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+            return List.of("png", "jpg", "jpeg").contains(extension);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
