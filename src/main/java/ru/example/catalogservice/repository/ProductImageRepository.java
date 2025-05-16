56239b198b105fc5049b13bff583280d7df71739
Yuri Luttsev <floomyz@vk.com>
2025-05-16 23:58:14 +0300

    Удалены методы из ProductImageRepository

--- src/main/java/ru/example/catalogservice/repository/ProductImageRepository.java
+++ src/main/java/ru/example/catalogservice/repository/ProductImageRepository.java
@@ -1,13 +1,9 @@
 package ru.example.catalogservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.example.catalogservice.model.entity.ProductImage;
 
-import java.util.List;
 import java.util.UUID;
 
-@Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, UUID> {
-    List<ProductImage> findByProductId(UUID productId);
 }
