955fe632c30cf144a4d02a371a297cac01a288b8
Yuri Luttsev <floomyz@vk.com>
2025-05-16 23:56:40 +0300

    Добавлен маппер для Product

--- src/main/java/ru/example/catalogservice/model/mapper/ProductMapper.java
+++ src/main/java/ru/example/catalogservice/model/mapper/ProductMapper.java
@@ -1,47 +1,16 @@
 package ru.example.catalogservice.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import ru.example.catalogservice.model.entity.Product;
-import ru.example.catalogservice.model.entity.ProductImage;
-import ru.example.catalogservice.model.payload.product.CreateProductRequest;
-import ru.example.catalogservice.model.payload.product.ProductResponse;
-import ru.example.catalogservice.model.payload.productimage.ProductImageResponse;
+import ru.example.catalogservice.model.payload.product.ProductPayload;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class ProductMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProductMapper {
 
-    public static Product toEntity(CreateProductRequest request) {
-        return Product.builder()
-                .name(request.name())
-                .price(request.price())
-                .discount(request.discount())
-                .categoryId(request.categoryId())
-                .isActive(true)
-                .build();
-    }
-
-    public static ProductResponse toResponse(Product entity) {
-        return new ProductResponse(
-                entity.getId(),
-                entity.getName(),
-                entity.getPrice(),
-                entity.getDiscount(),
-                entity.getCategoryId(),
-                entity.getCreatedAt(),
-                entity.getModifiedAt(),
-                entity.getIsActive(),
-                mapImagesToResponses(entity.getImages())
-        );
-    }
-
-    private static List<ProductImageResponse> mapImagesToResponses(List<ProductImage> images) {
-        if (images == null) {
-            return List.of();
-        }
-        return images.stream()
-                .filter(ProductImage::getIsActive)
-                .map(ProductImageMapper::toResponse)
-                .collect(Collectors.toList());
-    }
+    @Mapping(target = "images", source = "imagesUrls")
+    ProductPayload mapToProductPayload(Product product, List<String> imagesUrls);
 }
