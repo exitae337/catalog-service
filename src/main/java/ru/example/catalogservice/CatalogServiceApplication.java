8c34907c5fcbff4bb8b1e07e8e137b0b2974dc31
Yuri Luttsev <floomyz@vk.com>
2025-05-20 18:50:29 +0300

    Аннотации вынесены в отдельные конфигурации

--- src/main/java/ru/example/catalogservice/CatalogServiceApplication.java
+++ src/main/java/ru/example/catalogservice/CatalogServiceApplication.java
@@ -2,14 +2,8 @@ package ru.example.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableJpaAuditing
-@EnableCaching
 public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
