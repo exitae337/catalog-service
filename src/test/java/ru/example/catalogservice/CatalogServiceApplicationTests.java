2f12d47b0d35c0948d71abc52bb2b6dabd2e3ff1
Yuri Luttsev <floomyz@vk.com>
2025-05-20 18:52:40 +0300

    Добавлены тесты контекста

--- src/test/java/ru/example/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/ru/example/catalogservice/CatalogServiceApplicationTests.java
@@ -1,12 +1,37 @@
 package ru.example.catalogservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-public class CatalogServiceApplicationTests {
+@Testcontainers
+class CatalogServiceApplicationTests {
+
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17");
+
+    @Container
+    @ServiceConnection
+    private static KafkaContainer kafkaContainer = new KafkaContainer("apache/kafka:3.9.1");
+
+    @Container
+    private static GenericContainer<?> redisContainer = new GenericContainer<>("redis:8");
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", redisContainer::getHost);
+        registry.add("spring.redis.port", () -> redisContainer.getFirstMappedPort());
+    }
+
     @Test
     void contextLoads() {
     }
