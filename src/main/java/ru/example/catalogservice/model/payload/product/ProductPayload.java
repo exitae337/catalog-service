e1efc10a4ea4f89c3dd3b2e030e12b45e684823a
Yuri Luttsev <floomyz@vk.com>
2025-06-01 21:06:10 +0300

    Переименовано поле categoryId в JSON ProductPayload

--- src/main/java/ru/example/catalogservice/model/payload/product/ProductPayload.java
+++ src/main/java/ru/example/catalogservice/model/payload/product/ProductPayload.java
@@ -1,5 +1,7 @@
 package ru.example.catalogservice.model.payload.product;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
@@ -9,6 +11,8 @@ public record ProductPayload(
         UUID id,
         String name,
         BigDecimal price,
+
+        @JsonProperty("category_id")
         String categoryId,
         List<String> images
 ) implements Serializable {
