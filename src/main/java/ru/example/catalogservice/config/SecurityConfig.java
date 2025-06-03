7d7d0cacba69cb9e7563e3fc79769ef2ba6946e9
Yuri Luttsev <floomyz@vk.com>
2025-06-03 15:48:58 +0300

    Изменена конфигурация безопасности

--- src/main/java/ru/example/catalogservice/config/SecurityConfig.java
+++ src/main/java/ru/example/catalogservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ru.example.catalogservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,14 +18,24 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import ru.example.catalogservice.security.JwtAccessAuthenticationProvider;
 import ru.example.catalogservice.security.JwtAccessFilter;
 
+import java.util.Map;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
+    public Map<String, HttpMethod> securedEndpoints() {
+        return Map.of(
+                "/products", HttpMethod.POST
+        );
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
-                                                   JwtAccessFilter jwtAccessFilter) throws Exception {
+                                                   JwtAccessFilter jwtAccessFilter,
+                                                   Map<String, HttpMethod> securedEndpoints) throws Exception {
         return httpSecurity
                 .csrf(CsrfConfigurer::disable)
                 .cors(CorsConfigurer::disable)
@@ -33,10 +44,10 @@ public class SecurityConfig {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS
                 ))
-                .authorizeHttpRequests(customizer -> customizer
-                        .anyRequest()
-                        .authenticated()
-                )
+                .authorizeHttpRequests(customizer -> {
+                    securedEndpoints.forEach((path, method) -> customizer.requestMatchers(method, path).authenticated());
+                    customizer.anyRequest().permitAll();
+                })
                 .addFilterBefore(jwtAccessFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
